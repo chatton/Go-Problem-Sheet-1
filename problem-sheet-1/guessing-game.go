a3ff82ca0cb967938a0dc29996b5c2969ca8f8b4
chatton <cianhatton@gmail.com>
2017-09-16 21:30:55 +0100

    added guessing game solution

---
+++ problem-sheet-1/guessing-game.go
@@ -0,0 +1,46 @@
+
+/*
+Write a guessing game where the user must guess a secret number. 
+After every guess the program tells the user whether their number was too large or too small. 
+At the end the number of tries needed should be printed. 
+It counts only as one try if they input the same number multiple times consecutively.
+*/
+
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
+
+
+func displayMessage(secretNum, guessedNum, numGuesses int) {
+       if guessedNum == secretNum {
+            fmt.Println("You guessed the number correctly! It took you", numGuesses, "guesses")
+        } else if guessedNum < secretNum {
+            fmt.Println("Go higher!")
+        } else {
+            fmt.Println("Go lower!")
+        }
+}
+
+func main() {
+    // so we don't get the same number each time
+    rand.Seed(time.Now().UTC().UnixNano()) 
+    var secretNum = rand.Intn(100) + 1 // don't want to include 0
+    var numGuesses int = 0
+    var guess int = -1
+    for guess != secretNum {
+        lastGuess := guess
+        
+        fmt.Println("Guess a number between 1 and 100")
+        fmt.Scan(&guess)
+        
+        if lastGuess != guess {
+            numGuesses++
+        }
+
+        displayMessage(secretNum, guess, numGuesses)
+    }
+}
