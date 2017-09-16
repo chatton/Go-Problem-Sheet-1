d2a3f23abc9d01215e2e6cd7fb3cc5b700c1525f
chatton <cianhatton@gmail.com>
2017-09-16 07:42:55 +0100

    added hello sekai problem solution

---
+++ problem-sheet-1/konnichiwa-sekai.go
@@ -0,0 +1,13 @@
+/*
+Problem description
+
+Write a program that prints "Hello, world!" in Japanese (using Japanese characters) to the screen.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("こんにちは世界");    
+}
