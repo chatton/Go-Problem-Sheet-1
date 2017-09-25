bab40b1dbe6c35485006d1ab678f610a8ebbb610
chatton <cianhatton@gmail.com>
2017-09-25 12:48:17 +0100

    updated the reverse string function

--- problem-sheets/util/util.go
+++ problem-sheets/util/util.go
@@ -3,12 +3,12 @@ package util
 func Reverse(stringToReverse string) string {
     // based on this SO answer
     // https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go
-    length := len(stringToReverse)
-    runes := make([]rune, length) // make an array big enough to store all the characters
-    index := length
-    for _, rune := range stringToReverse {
-        index--
-        runes[index] = rune
+    runes := []rune(stringToReverse) // make an array full of the characters in the string
+    for i, rune := range stringToReverse {
+        // starts out at the length of the array
+        // -1 to start out at the last position.
+        // -i to move backwards through the positions.
+        runes[len(runes) - 1 - i] = rune
     }
-    return string(runes)
+    return string(runes) // convert the rune array back into a string.
 }
