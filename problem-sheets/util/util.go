c0a6a928c37809ea14ae962e7b5265be912492b5
chatton <cianhatton@gmail.com>
2017-09-26 22:22:51 +0100

    updated the Reverse function to account for 'non-standard' characters such as Japanese and Chinese characters

--- problem-sheets/util/util.go
+++ problem-sheets/util/util.go
@@ -5,14 +5,28 @@
 package util
 
 func Reverse(stringToReverse string) string {
-    // adapted from this SO answer
+    // I consulted this SO question
     // https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go
-    runes := []rune(stringToReverse) // make an array full of the characters in the string
-    for i, rune := range stringToReverse {
-        // starts out at the length of the array
-        // -1 to start out at the last position.
-        // -i to move backwards through the positions.
-        runes[len(runes) - 1 - i] = rune
-    }
-    return string(runes) // convert the rune array back into a string.
+    // in order to figure out how to account for the encodings in the reversal.
+    
+    // the reason you can't use len to get the number of characters
+    // is because it gives back the number of bytes, and different 
+    // characters can give back different numbers of bytes.
+
+    num := 0
+    runes := []rune(stringToReverse) // creates a new rune list that from the input string
+    for _, r := range stringToReverse { // don't care about the index, just the rune itself
+            runes[num] = r
+            num++ // the actual number of characters
+    } 
+
+    // Reverse the contents of the array by
+    // iterating to the halfway point and swapping elements
+    // on the furthest ends as you go.
+    for i := 0; i < num/2; i++ { 
+            runes[i], runes[num-1-i] = runes[num-1-i], runes[i] 
+    } 
+
+    // Finally, convert back to UTF-8. 
+    return string(runes)
 }
