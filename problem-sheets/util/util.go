e190e2f4c06ff13884bd51f8446c7bd4b4faa5fa
chatton <cianhatton@gmail.com>
2017-09-29 16:56:07 +0100

    updated the reverse string method to be more conscise

--- problem-sheets/util/util.go
+++ problem-sheets/util/util.go
@@ -9,6 +9,8 @@ import (
 	"os"
 	"strconv"
     "strings"
+    "unicode/utf8"
+
 )
 
 // can re-use in the different parsing functions instead of creating new one every time
@@ -19,35 +21,25 @@ func Reverse(stringToReverse string) string {
 	// https://stackoverflow.com/questions/1752414/how-to-reverse-a-string-in-go
 	// in order to figure out how to account for the encodings in the reversal.
 
-	// the reason you can't use len to get the number of characters
-	// is because it gives back the number of bytes, and different
-	// characters can give back different numbers of bytes.
-
-	num := 0
-	runes := []rune(stringToReverse)    // creates a new rune list that from the input string
-	for _, r := range stringToReverse { // don't care about the index, just the rune itself
-		runes[num] = r
-		num++ // the actual number of characters
-	}
-
-	// Reverse the contents of the array by
-	// iterating to the halfway point and swapping elements
-	// on the furthest ends as you go.
-	for i := 0; i < num/2; i++ {
-		runes[i], runes[num-1-i] = runes[num-1-i], runes[i]
-	}
-
-	// Finally, convert back to UTF-8.
-	return string(runes)
+    // get the number of runes in the string, default len function
+    // gives back the number of bytes, depending on the encoding a rune could be
+    // a different number of bytes.
+    numRunes := utf8.RuneCountInString(stringToReverse)
+    runes := []rune(stringToReverse)
+    // reverse the values in the rune slice.
+    for i := 0; i < num/2; i++ {
+        runes[i], runes[numRunes-1-i] = runes[numRunes-1-i], runes[i]
+    }
+    return string(runes) // convert the slice back into a string.
 }
 
 // simple function to sum up the values in an integer list
 func Sum(nums []int) int {
-	tally := 0
+	total := 0
 	for _, num := range nums {
-		tally += num
+		total += num
 	}
-	return tally
+	return total
 }
 
 // simple function to read a line in from the console.
