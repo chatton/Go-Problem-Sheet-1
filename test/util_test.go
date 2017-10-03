822479d2b21caed51874226601212c9bfe24ba04
chatton <cianhatton@gmail.com>
2017-10-03 16:43:38 +0100

    added test for AsIntSlice function

--- test/util_test.go
+++ test/util_test.go
@@ -46,3 +46,42 @@ func TestSum(t *testing.T) {
 	}
 
 }
+
+func TestAsIntSlice(t *testing.T) {
+	var testCases = []struct {
+		line   string
+		result []int
+	}{
+		{"1 2 3 4 5", []int{1, 2, 3, 4, 5}},
+		{"2 34 5 6", []int{2, 34, 5, 6}},
+		{"-2 32 -43 23", []int{-2, 32, -43, 23}},
+		{"-3 43 23 12 0 0 0", []int{-3, 43, 23, 12, 0, 0, 0}},
+	}
+
+	for _, testCase := range testCases {
+		if result, err := util.AsIntSlice(testCase.line); !compare(result, testCase.result) {
+			t.Errorf("Provided line %s, expected result was %v, actual result was %v. Error %s",
+				testCase.line, testCase.result, result, err)
+		}
+	}
+}
+
+// helper function to compare equality of 2 int slices
+func compare(slice1, slice2 []int) bool {
+	// can only compare slices to nil, not each other
+	if slice1 == nil || slice2 == nil {
+		return false
+	}
+
+	if len(slice1) != len(slice2) {
+		return false
+	}
+
+	// manually compare every element.
+	for index, _ := range slice1 {
+		if slice1[index] != slice2[index] {
+			return false
+		}
+	}
+	return true
+}
