f60b61134aad62e412fe9dee9c48c88f0c6970fb
chatton <cianhatton@gmail.com>
2017-10-03 16:19:17 +0100

    added test to reverse string

--- test/util_test.go
+++ test/util_test.go
@@ -1,28 +1,46 @@
 package main
 
 import (
-    "testing"
-    "../problem-sheets/util"
+	"../problem-sheets/util"
+	"testing"
 )
+
+func TestReverseString(t *testing.T) {
+	var testCases = []struct {
+		s, expected string
+	}{
+		{"Hello", "olleH"},
+		{"", ""},
+		{"World", "dlroW"},
+		{"こんにちは世界", "界世はちにんこ"},
+		{"mix こんにちは世界", "界世はちにんこ xim"},
+		{"هِجَائِي", "يِئاَجِه"},
+	}
+	for _, testCase := range testCases {
+		if result := util.Reverse(testCase.s); result != testCase.expected {
+			t.Errorf("String: [%s] - Actual [%s], expected [%s]", testCase.s, result, testCase.expected)
+		}
+	}
+}
 
 // function that tests the exported Sum function from the util package
 // used in the problem sheet.
-func TestSum(t *testing.T){
+func TestSum(t *testing.T) {
 
-    testCases := [][]int{
-        {1,2,3},
-        {4,5,6},
-        {-10, 23, 57},
-        {0,0,0},
-        {-10,20,32},
-    }
+	testCases := [][]int{
+		{1, 2, 3},
+		{4, 5, 6},
+		{-10, 23, 57},
+		{0, 0, 0},
+		{-10, 20, 32},
+	}
 
-    expectedResults := []int{6, 15, 70, 0, 42}
+	expectedResults := []int{6, 15, 70, 0, 42}
 
-    for index, testCase := range testCases {
-        if result := util.Sum(testCase); result != expectedResults[index]{
-            t.Errorf("Expected result %d, actual %d", expectedResults[index], result)
-        }
-    }
+	for index, testCase := range testCases {
+		if result := util.Sum(testCase); result != expectedResults[index] {
+			t.Errorf("Expected result %d, actual %d", expectedResults[index], result)
+		}
+	}
 
 }
